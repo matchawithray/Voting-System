8aeb237e97ddb9b88d72c81b783df92bac977c62
matchawithray <raymartbarberanbaylonfswg@gmail.com>
2026-08-15 06:33:23 +0000

    we list candidates

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Choose candidates: ")
+
+Candidate_Name["LAZATIN, CARMELLO BAUTISTA"]
+Candidate_Name2["GONZALES, AURELIO"]
+Candidate_Name3["SALI, IMRAKI SAMPUTOH"]
+if Candidate_Name == "1":
+    print("LAZATIN, CARMELLO BAUTISTA")
+else if Candidates_Name == "2":
+    print("GONZALES, AURELIO")
+else if Candidates_Name == "3":
+    print("SALI, IMRAKI SAMPUTOH")
+
+
+else
+print("Invalid input!")
+
+
+ 
