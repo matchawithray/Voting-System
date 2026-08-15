ab883245da7b10a3bad31274e1f0cdd8fa27dc7c
Mitchie-06 <galvezmitchie21@gmail.com>
2026-08-15 06:20:30 +0000

    Vote

---
+++ main.py
@@ -0,0 +1,17 @@
+Candidate_name = input ("Choose candidate: ")
+print("LAZATIN, CARMELO II BAUTISTA")
+print("GONZALES, AURELIO JR. DUEÑAS")
+
+class choice = input("Enter Choice: ")
+
+while class_choice not in ["1", "2"]:
+    print("Invalaid cjoice. Please choose again")
+    class_choice = input("Choose your candidate: \n1. Lazatin\n2. Gonzales")
+
+if class_choice == "1"
+    candidate_class = "Lazatin"
+
+elif class_choice == "2"
+    candidate_choice = "Gonzales"
+
+candidate = Candidate(candidate_name, candidate_class)
