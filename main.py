cd7aaf0ff87fdf9d8709a60c5ff4a07922f30432
matchawithray <raymartbarberanbaylonfswg@gmail.com>
2026-08-18 11:34:20 +0000

    added a if else condition

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ votes = [0, 0, 0, 0]
 for i, name in enumerate(candidates, 1):
     print(i, name)
 
-choice = int(input("Enter your choice: "))
-
+choice = int(input("Choose the candidates you want to vote: "))
+if 1 <= choice <= len(candidates):
+    votes[choice - 1] += 1
+    print("You voted for:", candidates[choice - 1])
+else:
+    print("Invalid choice.")
 
 
 
