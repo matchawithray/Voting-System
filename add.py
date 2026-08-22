3f955db9bbb2dd8b500d1c3f45694e57a16d107b
matchawithray <raymartbarberanbaylonfswg@gmail.com>
2026-08-22 02:42:25 +0000

    haaaaa

--- add.py
+++ add.py
@@ -1,26 +1,24 @@
-candidates = [     
+candidates = [
     {
-    "name": "Xyra Shannel",
-    "votes": 0
+        "name": "Xyra Shannel",
+        "votes": 0
     },
     {
-    "name": "Mitchie Mae",
-    "votes": 0
+        "name": "Mitchie Mae",
+        "votes": 0
     },
     {
-    "name": "Raymart Yu",
-    "votes": 0
+        "name": "Raymart Yu",
+        "votes": 0
     }
 ]
 
- new_candidate = input("Enter a new candidate name: ")
+new_candidate = input("Enter a new candidate name: ")
 
 candidates.append({
- "name": new_candidate,
- "votes": 0
-                                                                                    
+    "name": new_candidate,
+    "votes": 0
 })
 
 print(candidates)
-                                                                                    
-
+ 
